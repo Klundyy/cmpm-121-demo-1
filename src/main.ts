dc2eb3bb40f57f359a2571386a947de5a9e47e46
William Klunder <wklunder@ucsc.edu>
2024-10-14 12:02:40 -0700

    changed name added button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "My game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "click";
+button.style.backgroundColor = "black";
+button.style.webkitTextFillColor = "white";
+app.append(button);
